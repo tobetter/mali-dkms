Anonym3310/mali_kbase

linux: change irq prop names to lowercase
To match upstream DT bindings.

Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>

driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_core_linux.c
@@ -204,11 +204,11 @@ static int assign_irqs(struct platform_device *pdev)
 		}
 
 #ifdef CONFIG_OF
-		if (!strncmp(irq_res->name, "JOB", 4)) {
+		if (!strncmp(irq_res->name, "job", 4)) {
 			irqtag = JOB_IRQ_TAG;
-		} else if (!strncmp(irq_res->name, "MMU", 4)) {
+		} else if (!strncmp(irq_res->name, "mmu", 4)) {
 			irqtag = MMU_IRQ_TAG;
-		} else if (!strncmp(irq_res->name, "GPU", 4)) {
+		} else if (!strncmp(irq_res->name, "gpu", 4)) {
 			irqtag = GPU_IRQ_TAG;
 		} else {
 			dev_err(&pdev->dev, "Invalid irq res name: '%s'\n",

tobetter/mali-dkms

driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_core_linux.c
@@ -374,11 +374,11 @@ int assign_irqs(struct kbase_device *kbdev)
 		}
 
 #ifdef CONFIG_OF
-		if (!strncasecmp(irq_res->name, "JOB", 4)) {
+		if (!strncasecmp(irq_res->name, "job", 4)) {
 			irqtag = JOB_IRQ_TAG;
-		} else if (!strncasecmp(irq_res->name, "MMU", 4)) {
+		} else if (!strncasecmp(irq_res->name, "mmu", 4)) {
 			irqtag = MMU_IRQ_TAG;
-		} else if (!strncasecmp(irq_res->name, "GPU", 4)) {
+		} else if (!strncasecmp(irq_res->name, "gpu", 4)) {
 			irqtag = GPU_IRQ_TAG;
 		} else {
 			dev_err(&pdev->dev, "Invalid irq res name: '%s'\n",